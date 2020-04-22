a3b08e630473251ccc34bc36d236e642cff36dcf
Rick <ikbenrick@gmail.com>
2020-04-22 09:38:25 +0200

    Added infinite scroll list
    codelab part 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,73 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main()
-{
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Fast Tracker',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Simple Fast Tracker'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords>
+{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  // build function
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+    (
+      appBar: AppBar(title: Text('Simple Fast Tracker'),),
+      body: _buildSuggestions(),
+    );
+  }
+
+  _buildSuggestions()
+  {
+    return ListView.builder
+    (
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i)
+      {
+        if (i.isOdd)
+        {
+          return Divider();
+        }
+        
+        // add a wordpair
+        final index = i ~/ 2;
+        if (index >= _suggestions.length)
+        {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  
+  _buildRow(WordPair pair)
+  {
+    return ListTile
+    (
+      title: Text
+      (
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  //
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
-}
-
-class RandomWords extends StatefulWidget {
+class RandomWords extends StatefulWidget
+{
   @override
   RandomWordsState createState() => RandomWordsState();
 }
